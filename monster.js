ca7ec6e405c7a6b414e96f705d10233de099bf6f
d.nazdratenko@luckylabs.com <test>
2016-09-23 18:03:48 +0300

    some changes

--- monster.js
+++ monster.js
@@ -1,15 +1,14 @@
 const Position = {
-    START_X: 1920,
+    START_X: 1920 + 170,
     START_Y: 0,
     END_X: 0,
     END_Y: 770,
-    STEP_X: 11
+    STEP_X: 10
 };
 
 // Class Monster =======================================================================================================
 function Monster() {
     this.monster = null;
-    //this.speed = getRandomIntValue(Position.STEP_X/2, Position.STEP_X*2);
 }
 
 // Prototype createAnimation ===========================================================================================
@@ -27,7 +26,7 @@ Monster.prototype.createAnimation = function (monsterImages) {
     this.monster.x = Position.START_X;
     this.monster.y = getRandomIntValue(Position.START_Y, Position.END_Y);
     this.monster.scale.x = -1;
-    this.monster.width = 175;
+    this.monster.width = 170;
     this.monster.height = 140;
     this.monster.animationSpeed = 0.4;
 
