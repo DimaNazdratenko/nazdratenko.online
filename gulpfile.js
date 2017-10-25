a699af003bedcdeb3320311d30a64b1f1170e186
Dima <dima.nazdratenko@gmail.com>
2017-10-25 19:20:43 +0300

    Little changes. Add plugin for gulp and add bootstrap 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    cssnano = require("gulp-cssnano"),
+    rename = require("gulp-rename");
 
-gulp.task('sass', function () {
+
+gulp.task('css', function () {
     return gulp.src(['src/sass/*.sass', 'src/sass/*.scss'])
-        .pipe(sass({outputStyle: 'expanded'}).on('error',sass.logError))
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(cssnano({
+            zindex: false,
+            discardComments: {
+            removeAll: true
+            }
+        }))
+        .pipe(rename("style.min.css"))
         .pipe(gulp.dest('src/css'))
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['src/sass/**/*.sass', 'src/sass/**/*.scss'],['sass']);
+    gulp.watch(['src/sass/**/*.sass', 'src/sass/**/*.scss'], ['css']);
 });
 
 gulp.task('default', ['watch']);
