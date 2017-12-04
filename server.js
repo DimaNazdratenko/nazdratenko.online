8e98b68543c9c81bc844e0764be006f01397fe77
Dima <dima.nazdratenko@gmail.com>
2017-12-04 12:05:14 +0200

    delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var static = require('node-static');
-var file = new static.Server('.');
-
-http.createServer(function(req, res) {
-    file.serve(req, res);
-}).listen(8080);
-
-console.log('Server running on port 8080');
