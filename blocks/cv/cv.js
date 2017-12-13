e7f75ce416ff95ecbb5f7a9786304558daeac00f
Dima <dima.nazdratenko@gmail.com>
2017-12-13 20:09:49 +0200

    Little actualization

--- blocks/cv/cv.js
+++ blocks/cv/cv.js
@@ -1,6 +1,5 @@
 $.fn.extend({
     animateCss: function (animationName, callback) {
-        debugger;
         let animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
         this.addClass('animated ' + animationName).one(animationEnd, function () {
             $(this).removeClass('animated ' + animationName);
