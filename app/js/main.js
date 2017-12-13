34fab70bb5cd38dd8cb086435de0bc8f60cbdf8a
Dima <dima.nazdratenko@gmail.com>
2017-12-13 20:06:02 +0200

    Add animation for cv

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,35 @@
-// $(".cv_separator").on("click", function () {
-//     $(this)
-//         .addClass("cv_separator_animate")
-//         .animate({
-//             opacity: "0.1"
-//
-//         }, 1000, "linear")
-// });
+$.fn.extend({
+    animateCss: function (animationName, callback) {
+        debugger;
+        let animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function () {
+            $(this).removeClass('animated ' + animationName);
+            if (callback) {
+                callback();
+            }
+        });
+        return this;
+    }
+});
+
+$(function () {
+    let name = $(".cv_name"),
+        separatorOdd = $(".cv_separator:odd"),
+        separatorEven = $(".cv_separator:even"),
+        text = $(".cv_text");
+
+    name.css("visibility", "visible")
+        .animateCss("tada");
+
+    setTimeout(function () {
+        separatorOdd.css("visibility", "visible")
+            .animateCss("rollIn");
+        separatorEven.css("visibility", "visible")
+            .animateCss("rotateInUpRight");
+        text.css("visibility", "visible")
+            .animateCss("flip");
+    }, 1000);
+});
 
 // $("#test").on("click", function() {
 //     alert("ASDF")
