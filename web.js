3ad121f22f972cc23aa34ccaf6eddfdf4ec08c59
Dima Nazdratenko <d.nazdratenko@lucky-labs.com>
2018-01-02 16:55:34 +0200

    test for heroku

--- web.js
+++ web.js
@@ -3,5 +3,9 @@ let app = express();
 let path = require('path');
 
 app.use(express.static(path.join(process.cwd(), 'app')));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'app', 'index.html'));
+});
+
 console.log(process.env.PORT || 5000);
 app.listen(process.env.PORT || 5000);
