68b57b4d835cb14a4a5bfac5e4503539f468f5b1
Dima Nazdratenko <d.nazdratenko@lucky-labs.com>
2018-01-02 17:41:15 +0200

    little changes

--- web.js
+++ web.js
@@ -2,10 +2,13 @@ let express = require('express');
 let app = express();
 let path = require('path');
 
-app.use(express.static(path.join(process.cwd(), 'app')));
+app.use(express.static(path.join(__dirname, 'app')));
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'app', 'index.html'));
 });
+
+console.log('p', process.cwd());
 
 console.log(process.env.PORT || 5000);
 app.listen(process.env.PORT || 5000);
